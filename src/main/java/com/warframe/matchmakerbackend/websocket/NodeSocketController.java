974695be772ae848c8b600ab1a4c76bf5be7a897
Tom <tom-vautray@hotmail.fr>
2022-11-05 01:09:57 +0100

    WIP NodeSocketController

---
+++ src/main/java/com/warframe/matchmakerbackend/websocket/NodeSocketController.java
@@ -0,0 +1,26 @@
+package com.warframe.matchmakerbackend.websocket;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import com.warframe.matchmakerbackend.domain.Node;
+import com.warframe.matchmakerbackend.service.NodeService;
+
+@Controller
+public class NodeSocketController {
+	
+	@Autowired
+	private NodeService nodeService;
+	
+	@MessageMapping("/nodes/requestor")
+	@SendTo("/nodes/receptor")
+	public List<Node> nodes() throws Exception {
+		Thread.sleep(1000); // simulated delay
+		return nodeService.getNodes();
+	}
+
+}
