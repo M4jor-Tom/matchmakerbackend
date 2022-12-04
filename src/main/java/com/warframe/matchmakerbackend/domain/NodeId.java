3308b10865784d6a5775044ef621f80bdf1a2761
Tom <tom-vautray@hotmail.fr>
2022-12-04 23:22:23 +0100

    Add Earth nodes in NodeId, create mock data in NodeServiceImpl

--- src/main/java/com/warframe/matchmakerbackend/domain/NodeId.java
+++ src/main/java/com/warframe/matchmakerbackend/domain/NodeId.java
@@ -1,5 +1,19 @@
 package com.warframe.matchmakerbackend.domain;
 
 public enum NodeId {
-	NO_ID;
+	NO_ID,
+	MARIANA,
+	E_PRIME,
+	LITH,
+	ERPO,
+	EVEREST,
+	GAIA,
+	MANTLE,
+	CAMBRIA,
+	EURASIA,
+	PACIFIC,
+	CERVANTES,
+	COBA,
+	TIKAL,
+	ORO
 }
--- src/main/java/com/warframe/matchmakerbackend/service/impl/NodeServiceImpl.java
+++ src/main/java/com/warframe/matchmakerbackend/service/impl/NodeServiceImpl.java
@@ -11,17 +11,31 @@ import com.warframe.matchmakerbackend.service.NodeService;
 @Service
 public class NodeServiceImpl implements NodeService {
 
-	private long getWaitingPlayersCountForNode(NodeId nodeId) {
+	private Long getWaitingPlayersCountForNode(NodeId nodeId) {
 		//TODO Unmock
-		return 1;
+		return 1L;
 	}
 	
 	@Override
 	public List<NodeUpdate> getNodeUpdates() {
 		//TODO Unmock
 		return List.of(
-			new NodeUpdate(NodeId.NO_ID, getWaitingPlayersCountForNode(NodeId.NO_ID), true),
-			new NodeUpdate(NodeId.NO_ID, -1, false)
+			new NodeUpdate(NodeId.CAMBRIA, getWaitingPlayersCountForNode(NodeId.CAMBRIA), true),
+			new NodeUpdate(NodeId.CERVANTES, getWaitingPlayersCountForNode(NodeId.CERVANTES), true),
+			new NodeUpdate(NodeId.COBA, getWaitingPlayersCountForNode(NodeId.COBA), true),
+			new NodeUpdate(NodeId.E_PRIME, getWaitingPlayersCountForNode(NodeId.E_PRIME), true),
+			new NodeUpdate(NodeId.ERPO, getWaitingPlayersCountForNode(NodeId.ERPO), true),
+			new NodeUpdate(NodeId.EURASIA, getWaitingPlayersCountForNode(NodeId.EURASIA), true),
+//			new NodeUpdate(NodeId.EVEREST, getWaitingPlayersCountForNode(NodeId.EVEREST), true),
+			new NodeUpdate(NodeId.EVEREST, null, false),
+			new NodeUpdate(NodeId.GAIA, 2L, true),
+//			new NodeUpdate(NodeId.GAIA, getWaitingPlayersCountForNode(NodeId.GAIA), true),
+			new NodeUpdate(NodeId.LITH, getWaitingPlayersCountForNode(NodeId.LITH), true),
+			new NodeUpdate(NodeId.MANTLE, getWaitingPlayersCountForNode(NodeId.MANTLE), true),
+			new NodeUpdate(NodeId.MARIANA, getWaitingPlayersCountForNode(NodeId.MARIANA), true),
+			new NodeUpdate(NodeId.ORO, getWaitingPlayersCountForNode(NodeId.ORO), true),
+			new NodeUpdate(NodeId.PACIFIC, getWaitingPlayersCountForNode(NodeId.PACIFIC), true),
+			new NodeUpdate(NodeId.TIKAL, getWaitingPlayersCountForNode(NodeId.TIKAL), true)
 		);
 	}
 }
