141bd45c598c4afad4534a59328ff5f4bd3c3f2d
Tom <tom-vautray@hotmail.fr>
2022-12-12 20:42:17 +0100

    Success getting user name, can't send message back

--- src/main/java/com/warframe/matchmakerbackend/config/WebSocketConfig.java
+++ src/main/java/com/warframe/matchmakerbackend/config/WebSocketConfig.java
@@ -1,10 +1,17 @@
 package com.warframe.matchmakerbackend.config;
 
+import java.security.Principal;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -13,10 +20,37 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	  @Override
 	  public void configureMessageBroker(MessageBrokerRegistry config) {
 	    config.enableSimpleBroker("/ws-broker");
+        config.setUserDestinationPrefix("/secured/ws-user");
 	  }
 
 	  @Override
 	  public void registerStompEndpoints(StompEndpointRegistry registry) {
-	    registry.addEndpoint("/ws-endpoint").setAllowedOriginPatterns("*").withSockJS();
+	    registry.addEndpoint("/ws-endpoint")
+	    .setHandshakeHandler(new DefaultHandshakeHandler() {
+	    	// Custom class for storing principal
+	        @Override
+	        protected Principal determineUser(
+	            ServerHttpRequest request,
+	            WebSocketHandler wsHandler,
+	            Map<String, Object> attributes
+	        ) {
+	            // Generate principal with UUID as name
+	            return new StompPrincipal(UUID.randomUUID().toString());
+	        }
+	    })
+	    .setAllowedOriginPatterns("*").withSockJS();
 	  }
+	  
+	  private class StompPrincipal implements Principal {
+      	private String name;
+
+        public StompPrincipal(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+    }
 }
--- src/main/java/com/warframe/matchmakerbackend/controller/NodeController.java
+++ src/main/java/com/warframe/matchmakerbackend/controller/NodeController.java
@@ -1,13 +1,18 @@
 package com.warframe.matchmakerbackend.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.warframe.matchmakerbackend.domain.Action;
 import com.warframe.matchmakerbackend.service.NodeService;
 
 @Controller
@@ -18,11 +23,20 @@ public class NodeController {
 	@Autowired
 	private NodeService nodeService;
 	
-	@MessageMapping("/ws-destination")
-	@SendTo("/ws-broker")
-	public String getUpdatedNode(String actionJson) throws Exception {
-		Action action = GSON.fromJson(actionJson, Action.class);
-		System.out.println(action);
-		return GSON.toJson(nodeService.getNodeUpdates());
+	@Autowired
+	private SimpMessagingTemplate simpMessagingTemplate;
+
+	@MessageMapping("/secured/ws-room")
+	@SendToUser("/secured/ws-user")
+	public void sendSpecific(
+	  @Payload Message msg,
+	  Principal user, 
+	  @Header("simpSessionId") String sessionId) throws Exception { 
+		System.out.println("SessionId: " + sessionId);
+		System.out.println("Message: " + msg);
+		System.out.println("userName: " + user.getName());
+	    String message = GSON.toJson(nodeService.getNodeUpdates());
+//	    simpMessagingTemplate.convertAndSendToUser(user.getName(), "/secured/ws-user", msg);
+	    simpMessagingTemplate.convertAndSendToUser(user.getName(), "/secured/ws-user" , message);
 	}
 }
